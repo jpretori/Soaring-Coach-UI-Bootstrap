5b9d3cfca4fdf86ccf25718c9733091f6384b1f5
Johan Pretorius <pretoriusjf@gmail.com>
2017-03-10 11:15:19 +0200

    Corrected the endpoint this calls

--- dist/js/soaringcoach.js
+++ dist/js/soaringcoach.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://protected-bayou-34428.herokuapp.com/upload"
+        url: "https://protected-bayou-34428.herokuapp.com/health"
     }).then(function(data) {
        $('.bearing').append(data.bearing);
        $('.size').append(data.size);
