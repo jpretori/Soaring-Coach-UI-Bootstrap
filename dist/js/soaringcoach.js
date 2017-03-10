c6ab0e1afb42ed8b892b935d50d0c18f6c812011
Johan Pretorius <pretoriusjf@gmail.com>
2017-03-10 15:00:19 +0200

    testing

--- dist/js/soaringcoach.js
+++ dist/js/soaringcoach.js
@@ -1,3 +1,4 @@
+//Testing REST calls
 $(document).ready(function() {
     $.ajax({
         type: 'GET',
@@ -5,8 +6,8 @@ $(document).ready(function() {
         contentType: "application/json; charset=utf-8",
         url: "https://protected-bayou-34428.herokuapp.com/health?echo=21",
         success: function(data){
-          console.log(data.bearing);
-          console.log(data.size);
+          $('#bearing').append(data.bearing);
+          $('#size').append(data.size);
             },
         error : function(data){
                     console.log(data.bearing);
@@ -14,3 +15,34 @@ $(document).ready(function() {
                    }
     })
 });
+
+//Upload flight & list thermals
+// $(document).ready(function() {
+//     $.("#fileUpload").click(function(){
+//       $.post("https://protected-bayou-34428.herokuapp.com/upload",
+//       $("#fileInput").val(),
+//       function(data,status){
+//
+//       });
+//             });
+//     })
+// });
+
+//Examples for setting various bits of the DOM
+// $(document).ready(function(){
+//     $("#btn1").click(function(){
+//         $("#test1").text("Hello world!");
+//     });
+//     $("#btn2").click(function(){
+//         $("#test2").html("<b>Hello world!</b>");
+//     });
+//     $("#btn3").click(function(){
+//         $("#test3").val("Dolly Duck");
+//     });
+//     $("button").click(function(){
+//         $("#w3s").attr({
+//             "href" : "https://www.w3schools.com/jquery",
+//             "title" : "W3Schools jQuery Tutorial"
+//         });
+//     });
+// });
