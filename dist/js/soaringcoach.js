d1847f27713b42e8cb3528cd70f0c8e20f730ad4
Johan Pretorius <pretoriusjf@gmail.com>
2017-03-10 11:24:06 +0200

    Added parameter to endpoint call

--- dist/js/soaringcoach.js
+++ dist/js/soaringcoach.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://protected-bayou-34428.herokuapp.com/health"
+        url: "https://protected-bayou-34428.herokuapp.com/health?echo=21"
     }).then(function(data) {
        $('.bearing').append(data.bearing);
        $('.size').append(data.size);
