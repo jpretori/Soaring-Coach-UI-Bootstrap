84e4946d77740ee3bfcfeb994c3f7300826c71a6
Johan Pretorius <pretoriusjf@gmail.com>
2017-03-14 22:11:44 +0200

    Finally got file upload to REST service working.
    Not doing anything with the response yet, but the file upload succeeds.

--- dist/js/soaringcoach.js
+++ dist/js/soaringcoach.js
@@ -1,48 +1,22 @@
-//Testing REST calls
-$(document).ready(function() {
-    $.ajax({
-        type: 'GET',
-        crossDomain: true,
-        contentType: "application/json; charset=utf-8",
-        url: "https://protected-bayou-34428.herokuapp.com/health?echo=21",
-        success: function(data){
-          $('#bearing').append(data.bearing);
-          $('#size').append(data.size);
-            },
-        error : function(data){
-                    console.log(data.bearing);
-                    console.log(data.size);
-                   }
-    })
+$("#uploadForm").submit(function(event){
+   //Do stuff.  Like upload the file.
+   var form = new FormData($('#uploadForm')[0]);
+
+   var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://protected-bayou-34428.herokuapp.com/upload",
+      //"url": "http://localhost:9876/upload",
+      "type": "POST",
+      "cache": false,
+      "processData": false,
+      "contentType": false,
+      "data": form
+   }
+
+   $.ajax(settings).done(function (response) {
+     console.log(response);
+   });
+
+   event.preventDefault();
 });
-
-//Upload flight & list thermals
-// $(document).ready(function() {
-//     $.("#fileUpload").click(function(){
-//       $.post("https://protected-bayou-34428.herokuapp.com/upload",
-//       $("#fileInput").val(),
-//       function(data,status){
-//
-//       });
-//             });
-//     })
-// });
-
-//Examples for setting various bits of the DOM
-// $(document).ready(function(){
-//     $("#btn1").click(function(){
-//         $("#test1").text("Hello world!");
-//     });
-//     $("#btn2").click(function(){
-//         $("#test2").html("<b>Hello world!</b>");
-//     });
-//     $("#btn3").click(function(){
-//         $("#test3").val("Dolly Duck");
-//     });
-//     $("button").click(function(){
-//         $("#w3s").attr({
-//             "href" : "https://www.w3schools.com/jquery",
-//             "title" : "W3Schools jQuery Tutorial"
-//         });
-//     });
-// });
