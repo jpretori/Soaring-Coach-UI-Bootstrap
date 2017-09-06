f5c274c9e43f0d02178b2066dd29a1310ed24bd1
Johan Pretorius <pretoriusjf@gmail.com>
2017-09-06 22:12:35 +0200

    Vastly improve layout and visual polish
     - Added file loading spinner
     - Laid out text elements in single row
     - Laid out two graphical elements on their own row
     - Removed ugly and less-valuable straight phase detail
     - Changed opacity on open-source footer

--- dist/js/soaringcoach.js
+++ dist/js/soaringcoach.js
@@ -1,4 +1,7 @@
 $("#uploadForm").submit(function(event){
+   $('#flightDetails').fadeOut();
+   spinner = startSpinner();
+
    var form = new FormData($('#uploadForm')[0]);
 
    var settings = {
@@ -18,7 +21,8 @@ $("#uploadForm").submit(function(event){
 
      $('#pilotName').html(response.pilotName);
 
-     $('#totalDistance').html(response.totalGroundTrackDistance);
+     distKm = response.totalGroundTrackDistance / 1000;
+     $('#totalDistance').html(distKm.toLocaleString() + " km");
 
      $('#flightDate').html(response.flightDate);
 
@@ -26,6 +30,9 @@ $("#uploadForm").submit(function(event){
 
      plotStraightPhases(response);
      $('#straightPhases').html(getStraightPhasesList(response));
+
+     spinner.stop();
+     $('#flightDetails').fadeIn();
    });
 
    event.preventDefault();
@@ -130,4 +137,32 @@ function getStraightPhasesList(response) {
     "</li></ul>";
 
     return html;
+}
+
+function startSpinner() {
+   var opts = {
+      lines: 13 // The number of lines to draw
+      , length: 28 // The length of each line
+      , width: 14 // The line thickness
+      , radius: 42 // The radius of the inner circle
+      , scale: 1 // Scales overall size of the spinner
+      , corners: 1 // Corner roundness (0..1)
+      , color: '#000' // #rgb or #rrggbb or array of colors
+      , opacity: 0.25 // Opacity of the lines
+      , rotate: 0 // The rotation offset
+      , direction: 1 // 1: clockwise, -1: counterclockwise
+      , speed: 1 // Rounds per second
+      , trail: 60 // Afterglow percentage
+      , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+      , zIndex: 2e9 // The z-index (defaults to 2000000000)
+      , className: 'spinner' // The CSS class to assign to the spinner
+      , top: '50%' // Top position relative to parent
+      , left: '50%' // Left position relative to parent
+      , shadow: false // Whether to render a shadow
+      , hwaccel: false // Whether to use hardware acceleration
+      , position: 'absolute' // Element positioning
+   }
+   var target = document.getElementById('uploadForm')
+   var spinner = new Spinner(opts).spin(target);
+   return spinner;
 }
